0d5ddcee7920b13f54067c75ae972a93ff9f822b
unknown <esdesuagk@gmail.com>
2023-08-17 12:54:52 +0300

    routes

--- app/Http/Controllers/API/GenreController.php
+++ app/Http/Controllers/API/GenreController.php
@@ -1,52 +1,52 @@
-<?php
+    <?php
 
-namespace App\Http\Controllers\API;
+    namespace App\Http\Controllers\API;
 
-use App\Models\Genre;
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+    use App\Models\Genre;
+    use App\Http\Controllers\Controller;
+    use Illuminate\Http\Request;
 
-class GenreController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    class GenreController extends Controller
     {
-        $genres = Genre::paginate(3);
-        return response()->json($genres);
-    }
+        /**
+         * Display a listing of the resource.
+         */
+        public function index()
+        {
+            $genres = Genre::paginate(3);
+            return response()->json($genres);
+        }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        /**
+         * Store a newly created resource in storage.
+         */
+        public function store(Request $request)
+        {
+            //
+        }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        $genre = Genre::findOrFail($id);
-        return response()->json($genre->movies->paginate(3));
-    }
+        /**
+         * Display the specified resource.
+         */
+        public function show(string $id)
+        {
+            $genre = Genre::findOrFail($id);
+            return response()->json($genre->movies()->paginate(3));
+        }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+        /**
+         * Update the specified resource in storage.
+         */
+        public function update(Request $request, string $id)
+        {
+            //
+        }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        /**
+         * Remove the specified resource from storage.
+         */
+        public function destroy(string $id)
+        {
+            //
+        }
     }
-}
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\MovieController;
 use App\Http\Controllers\API\GenreController;
 
-Route::get('/genres', [GenreController::class, 'index']);
-Route::get('/genres/{id}', [MovieController::class, 'show']);
-Route::get('/movies', [MovieController::class, 'index']);
-Route::get('/movies/{id}', [MovieController::class, 'show']);
+Route::get('/genres', [GenreController::class, 'index'])->name('genres.index');
+Route::post('/genres', [GenreController::class, 'store'])->name('genres.store');
+Route::get('/genres/{id}', [MovieController::class, 'show'])->name('genres.show');
+Route::patch('/genres/{id}', [GenreController::class, 'update'])->name('genres.update');
+Route::delete('/genres/{id}', [GenreController::class, 'destroy'])->name('genres.destroy');
+
+Route::get('/movies', [MovieController::class, 'index'])->name('movies.index');
+Route::post('/movies', [MovieController::class, 'store'])->name('movies.store');
+Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.show');
+Route::patch('/movies/{id}', [MovieController::class, 'update'])->name('movies.update');
+Route::delete('/movies/{id}', [MovieController::class, 'destroy'])->name('movies.destroy');
