b669f4851c3152131a377e0cb55039e2329c24b6
unknown <esdesuagk@gmail.com>
2023-08-17 18:02:36 +0300

    docs comments in controllers, genres in response MovieController

--- app/Http/Controllers/API/GenreController.php
+++ app/Http/Controllers/API/GenreController.php
@@ -9,17 +9,33 @@ use Illuminate\Support\Facades\Validator;
 
 class GenreController extends Controller
 {
-
+    
+    /**
+     * rulesValidation
+     *
+     * @var array
+     */
     private $rulesValidation = [
         'name' => ['required', 'string', 'min: 3', 'max:30'],
     ];
-
+    
+    /**
+     * index
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index()
     {
         $genres = Genre::paginate(3);
         return response()->json($genres);
     }
-
+    
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function store(Request $request)
     {
         $validator = Validator::make($request->json()->all(), $this->rulesValidation);
@@ -31,13 +47,26 @@ class GenreController extends Controller
 
         return response()->json($genre);
     }
-
+    
+    /**
+     * show
+     *
+     * @param  mixed $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function show(string $id)
     {
         $genre = Genre::findOrFail($id);
         return response()->json($genre->movies()->paginate(3));
     }
-
+    
+    /**
+     * update
+     *
+     * @param  mixed $request
+     * @param  mixed $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function update(Request $request, string $id)
     {
         $validator = Validator::make($request->json()->all(), $this->rulesValidation);
@@ -50,7 +79,13 @@ class GenreController extends Controller
 
         return response()->json($genre);
     }
-
+    
+    /**
+     * destroy
+     *
+     * @param  mixed $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function destroy(string $id)
     {
         $genre = Genre::findOrFail($id);
--- app/Http/Controllers/API/MovieController.php
+++ app/Http/Controllers/API/MovieController.php
@@ -9,20 +9,36 @@ use Illuminate\Support\Facades\Validator;
 
 class MovieController extends Controller
 {
-
+    
+    /**
+     * rulesValidation
+     *
+     * @var array
+     */
     private $rulesValidation = [
         'title' => ['required', 'string', 'min: 2','max:35'],
         'publication_status' => ['boolean'],
         'poster_link' => ['string'],
         'genres' => ['required']
     ];
-
+    
+    /**
+     * index
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index()
     {
-        $movies = Movie::paginate(3);
+        $movies = Movie::with('genres')->paginate(3);
         return response()->json($movies);
     }
-
+    
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function store(Request $request)
     {
         $validator = Validator::make($request->json()->all(), $this->rulesValidation);
@@ -36,19 +52,38 @@ class MovieController extends Controller
 
         return response()->json($movie);
     }
-
+    
+    /**
+     * publish
+     *
+     * @param  mixed $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function publish(string $id)
     {
         $movie = Movie::find($id);
         return $movie->publish();
     }
-
+    
+    /**
+     * show
+     *
+     * @param  mixed $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function show(string $id)
     {
-        $movie = Movie::findOrFail($id);
+        $movie = Movie::with('genres')->findOrFail($id);
         return response()->json($movie);
     }
-
+    
+    /**
+     * update
+     *
+     * @param  mixed $request
+     * @param  mixed $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function update(Request $request, string $id)
     {
         $validator = Validator::make($request->json()->all(), $this->rulesValidation);
@@ -65,7 +100,13 @@ class MovieController extends Controller
 
         return response()->json($movie);
     }
-
+    
+    /**
+     * destroy
+     *
+     * @param  mixed $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function destroy(string $id)
     {
         $movie = Movie::findOrFail($id);
